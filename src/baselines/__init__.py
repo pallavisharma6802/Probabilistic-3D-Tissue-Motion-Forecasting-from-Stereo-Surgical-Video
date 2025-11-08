aec45c2a5f78cdbb760e737b3a42f10f5a0d3f40
Pallavi <pallavisharma6802@gmail.com>
2025-11-07 18:45:40 -0600

    Clean slate: Remove all Hamlyn code, ready for SCARED dataset

--- src/baselines/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Baseline models for depth estimation.
-"""
-
-from .raft_stereo_baseline import RAFTStereoBaseline
-
-__all__ = ['RAFTStereoBaseline']
--- src/instrumentation/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Instrumentation module for measuring system latency and performance"""
-
-from .latency_profiler import SystemLatencyProfiler, TimingMeasurement, profile_dummy_model
-
-__all__ = ['SystemLatencyProfiler', 'TimingMeasurement', 'profile_dummy_model']
